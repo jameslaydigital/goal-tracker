a497e6968319b5899ccdd55aa35415b74d6570b3
james lay <jameslaymusic@gmail.com>
2026-07-15 16:37:38 -0600

    prepare for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/goal-tracker/',
   plugins: [
     react(),
     tailwindcss(),
